1f588ec6272c7580fa04efd176c87a23937c5ad7
Rodrigo Grisi <46337889+RodrigoGrisi@users.noreply.github.com>
2023-12-26 15:16:12 -0300

    função tarefa ok

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-let listElement = document.querySelector('#App ul')
-let inputElement = document.querySelector('#app input')
-let buttonElement = document.querySelector('#app Button')
+let listElement = document.querySelector("#app ul");
+let inputElement = document.querySelector("#app input");
+let buttonElement = document.querySelector("#app button");
 
+let tarefas = [];
+
+function renderTarefas() {
+  listElement.innerHTML = "";
+  tarefas.map((todo) => {
+
+    liElement = document.createElement('li')
+    textTarefa = document.createTextNode(todo)
+    liElement.appendChild(textTarefa)
+    listElement.appendChild(liElement)
+
+  })
+}
+
+
+function adicionarTarefas() {
+  if (inputElement.value == '') {
+    alert("Digite uma tarefa.")
+    return false;
+  } else {
+    let novaTarefa = inputElement.value;
+    tarefas.push(novaTarefa);
+    inputElement.value = "";
+    renderTarefas()
+  }
+}
+
+buttonElement.onclick = adicionarTarefas;
